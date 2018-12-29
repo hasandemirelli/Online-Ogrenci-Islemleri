f250c1a9775ffb72d870401945279bfcc770a753
ferhatbostanci <ferhatbostanciofficial@gmail.com>
2018-12-30 00:47:01 +0300

    Yeni tema eklendi

--- login.php
+++ login.php
@@ -1,30 +1,67 @@
+<!DOCTYPE html>
+<html lang="tr">
 
-<?php
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Online Öğrenci İşlemleri - Giriş</title>
 
+    <link rel="shortcut icon" href="images/favicon.png" />
 
-try{
-    $baglanti = new PDO("mysql:host=127.0.0.1:3306;dbname=ogrenci","root","12345");
-} catch(PDOException $e){
-    print $e->getMessage();
-}
-session_start();
+    <link rel="stylesheet" href="vendors/iconfonts/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="vendors/css/vendor.bundle.addons.css">
 
+    <link rel="stylesheet" href="css/style.css">
+</head>
 
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper auth-page">
+      <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
+        <div class="row w-100">
+          <div class="col-lg-4 mx-auto">
+            <div class="auto-form-wrapper">
+              <form action="#">
+                <div class="form-group">
+                  <label class="label">Kullanıcı Adı</label>
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Kullanıcı Adı">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="label">Parola</label>
+                  <div class="input-group">
+                    <input type="password" class="form-control" placeholder="*********">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-primary submit-btn btn-block">Giriş Yap</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-$email=filter_input(INPUT_POST, 'email');
-$password=filter_input(INPUT_POST, 'password');
+  <script src="vendors/js/vendor.bundle.base.js"></script>
+  <script src="vendors/js/vendor.bundle.addons.js"></script>
 
-$sorgu = $baglanti -> query("SELECT * FROM ogr_table WHERE  ogr_tc =$password && ogr_mail='$email'" ) -> fetch(PDO:: FETCH_ASSOC);
-	if($sorgu){
-		
-		header ("Location:anasayfa.php"); 
-	}
-	else{
-		
-		header ("Location:index.php"); 
-		
-	}
-	
-	
-?>
+  <script src="js/off-canvas.js"></script>
+  <script src="js/misc.js"></script>
 
+</body>
+
+</html>
