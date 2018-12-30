b8191036c9a63d32e2857dfeb8c10f48144bc272
ferhatbostanci <ferhatbostanciofficial@gmail.com>
2018-12-30 18:12:04 +0300

    Login duzenlendi

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
+<?php
+
+// DB baglantisi
+try{
+    $db = new PDO("mysql:host=db4free.net:3306;dbname=fh_online_ogr", "fh_online_user", "fh159357");
+}catch(PDOException $e){
+    print $e->getMessage();
+}
+
+// Eger eposta ve sifre varsa islem yaptir
+if(isset($_POST['eposta'], $_POST['sifre']))
+
+?>
+
 <!DOCTYPE html>
 <html lang="tr">
 
@@ -6,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Online Öğrenci İşlemleri - Giriş</title>
 
-    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="shortcut icon" href="images/favicon.png"/>
 
     <link rel="stylesheet" href="vendors/iconfonts/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css">
@@ -16,51 +30,53 @@
 </head>
 
 <body>
-  <div class="container-scroller">
+<div class="container-scroller">
     <div class="container-fluid page-body-wrapper full-page-wrapper auth-page">
-      <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
-        <div class="row w-100">
-          <div class="col-lg-4 mx-auto">
-            <div class="auto-form-wrapper">
-              <form action="#">
-                <div class="form-group">
-                  <label class="label">Kullanıcı Adı</label>
-                  <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Kullanıcı Adı">
-                    <div class="input-group-append">
-                      <span class="input-group-text">
-                        <i class="mdi mdi-check-circle-outline"></i>
-                      </span>
+        <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
+            <div class="row w-100">
+                <div class="col-lg-4 mx-auto">
+                    <div class="auto-form-wrapper">
+                        <form action="login.php" method="POST">
+                            <div class="form-group">
+                                <label class="label">Kullanıcı Adı</label>
+                                <div class="input-group">
+                                    <input type="text" name="eposta" class="form-control" placeholder="Kullanıcı Adı"
+                                           value="<?php echo (isset($_POST['eposta'])) ? $_POST['eposta'] : '' ?>">
+                                    <div class="input-group-append">
+                                      <span class="input-group-text">
+                                        <i class="mdi mdi-check-circle-outline"></i>
+                                      </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="label">Parola</label>
+                                <div class="input-group">
+                                    <input type="password" name="sifre" class="form-control" placeholder="*********"
+                                           value="<?php echo (isset($_POST['sifre'])) ? $_POST['sifre'] : '' ?>">
+                                    <div class="input-group-append">
+                                      <span class="input-group-text">
+                                        <i class="mdi mdi-check-circle-outline"></i>
+                                      </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btn btn-primary submit-btn btn-block" value="Giriş Yap">
+                            </div>
+                        </form>
                     </div>
-                  </div>
                 </div>
-                <div class="form-group">
-                  <label class="label">Parola</label>
-                  <div class="input-group">
-                    <input type="password" class="form-control" placeholder="*********">
-                    <div class="input-group-append">
-                      <span class="input-group-text">
-                        <i class="mdi mdi-check-circle-outline"></i>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <button class="btn btn-primary submit-btn btn-block">Giriş Yap</button>
-                </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 
-  <script src="vendors/js/vendor.bundle.base.js"></script>
-  <script src="vendors/js/vendor.bundle.addons.js"></script>
+<script src="vendors/js/vendor.bundle.base.js"></script>
+<script src="vendors/js/vendor.bundle.addons.js"></script>
 
-  <script src="js/off-canvas.js"></script>
-  <script src="js/misc.js"></script>
+<script src="js/off-canvas.js"></script>
+<script src="js/misc.js"></script>
 
 </body>
 
